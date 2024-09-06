9e26dd9a8d1fcd6bcf96e13858d19bed90dbb96e
twonklez <49579028+twonklez@users.noreply.github.com>
2024-09-06 15:35:59 -0700

    Added State Machine
    State Machine in player controller

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,6 +2,15 @@ extends CharacterBody2D
 
 
 const speed = 300.0
+var is_interacting: bool = false
+
+var current_state: State = State.IDLE # Default to IDLE STATE
+
+
+enum State {
+	IDLE,
+	INTERACT
+}
 
 	
 func get_input():
@@ -12,3 +21,17 @@ func _physics_process(delta: float) -> void:
 	get_input()
 	
 	move_and_slide()
+	
+	# Handle animation 
+	match current_state:
+		State.INTERACT:
+			#Play Ineraction animations
+			pass
+	match current_state:
+		State.IDLE:
+			pass
+			
+
+func interact():
+	current_state = State.INTERACT 
+	is_interacting = true
