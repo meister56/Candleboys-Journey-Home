13ee71885102c5af2441e49a91588b6733fd1cec
meister <90567661+meister56@users.noreply.github.com>
2024-09-08 12:10:27 +0200

    lalalalal

--- Scripts/Monster.gd
+++ Scripts/Monster.gd
@@ -12,7 +12,8 @@ func _physics_process(delta: float) -> void:
 	if player != null:
 		if player.is_lit || global_position.distance_to(player.global_position) < 300:
 			velocity = direction * move_speed
-		
+		else:
+			velocity = lerp(velocity,Vector2.ZERO,delta)
 	move_and_slide()
 	
 func _on_bad_guy_death_zone_body_entered(body: Node2D) -> void:
