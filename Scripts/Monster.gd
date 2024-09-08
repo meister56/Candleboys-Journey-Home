3ec7fc3c824520a02542c99695d79e504ab02a6e
meister <90567661+meister56@users.noreply.github.com>
2024-09-08 16:25:08 +0200

    avoidance

--- Scripts/Monster.gd
+++ Scripts/Monster.gd
@@ -2,17 +2,24 @@ extends CharacterBody2D
 class_name Monster
 
 
-var move_speed:float = 200
+var move_speed:float = 180
 
 @onready var player = get_tree().get_first_node_in_group("Player")
 var tp_rate:float = 15
 var tp_time:float
 
 func _ready() -> void:
-	
+	add_to_group("Enemy")
 	randomize()
 func _physics_process(delta: float) -> void:
 	
+	
+	for enemy in get_tree().get_nodes_in_group("Enemy"):
+		
+		if enemy.global_position.distance_to(global_position) < 300 && enemy != self:
+			
+			velocity += enemy.global_position.direction_to(global_position) * 10
+			
 	if player != null:
 		var direction:Vector2 = global_position.direction_to(player.global_position)
 		if player.is_lit || global_position.distance_to(player.global_position) < 500:
